b31c26ead7883395029b0b7f9e9e4f6ec3b5ccbd
duhow <contacto@duhowpi.net>
2021-08-30 18:59:59 +0200

    Fix function call

--- src/main.js
+++ src/main.js
@@ -8,33 +8,34 @@ async function run() {
   const octokit = github.getOctokit(token);
 
   let repository = core.getInput('repository');
-  if (!repository) { repository = `{context.repo.owner}/{context.repo.repo}`; }
+  if (!repository) { repository = '{context.repo.owner}/{context.repo.repo}'; }
   core.info(`Using GitHub repository: ${repository}`);
 
   const [owner, repo] = repository.split('/');
-  core.debug(`owner: ${owner}, repo: ${repo}.`)
+  core.debug(`owner: ${owner}, repo: ${repo}.`);
 
   const tag = core.getInput('tag');
   const releaseId = core.getInput('release-id');
 
+  let release = null;
   if (tag) {
     core.info(`Getting release by tag: ${tag}`);
-    const { data: release } = await octokit.rest.repos.getReleaseByTag(owner, repo, tag);
+    release = await octokit.rest.repos.getReleaseByTag({ owner, repo, tag });
   } else if (releaseId && releaseId === 'latest') {
     core.info('Getting latest release');
-    const { data: release } = await octokit.rest.repos.getLatestRelease(owner, repo);
+    release = await octokit.rest.repos.getLatestRelease({ owner, repo });
   } else if (releaseId) {
     core.info(`Getting release ID: ${releaseId}`);
-    const { data: release } = await octokit.rest.repos.getRelease(owner, repo, releaseId);
+    release = await octokit.rest.repos.getRelease({ owner, repo, releaseId });
   } else {
     core.setFailed('No valid tag or release ID provided.');
   }
 
+  core.info(release);
+
   if (!release) {
     core.setFailed('Release does not exist or is unaccessible.');
   }
-
-  console.log(release);
 }
 
 run();
