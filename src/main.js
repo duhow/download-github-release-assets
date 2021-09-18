1a249c97d2f1858f10a4e7dd3655f3f5204e5c1e
David Girón <contacto@duhowpi.net>
2021-09-18 11:55:59 +0200

    Code base working (#1)
    * Adding a small Github Actions Workflow to run and self-test script.
    * Lint is not running automatic since I have couple of no-restricted-syntax warnings.
    * Adding default values in Actions with.
    * Global try-catch should work in few cases.

--- src/main.js
+++ src/main.js
@@ -1,47 +1,125 @@
+const fs = require('fs');
 const core = require('@actions/core');
 const github = require('@actions/github');
+
 // const glob = require('@actions/glob')
 
-async function run() {
-  const { context } = github;
-  const token = core.getInput('token');
-  let repository = core.getInput('repository');
+const token = core.getInput('token');
+const octokit = github.getOctokit(token);
 
-  let octokit;
+function regExpEscape(s) { return s.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&'); }
+function wildcardToRegExp(s) { return new RegExp(`^${s.split(/\*+/).map(regExpEscape).join('.*')}$`); }
+
+async function run() {
   try {
-    octokit = github.getOctokit(token);
+    const [owner, repo] = core.getInput('repository', { required: true }).split('/');
+    core.info(`Using GitHub repository: ${owner}/${repo}`);
+
+    const tag = core.getInput('tag');
+    const releaseId = core.getInput('release-id');
+    const selectorFiles = core.getInput('files').split('\n');
+
+    let release = null;
+    if (tag) {
+      core.info(`Getting release by tag: ${tag}`);
+      release = await octokit.rest.repos.getReleaseByTag({ owner, repo, tag });
+    } else if (releaseId && releaseId === 'latest') {
+      core.info('Getting latest release');
+      release = await octokit.rest.repos.getLatestRelease({ owner, repo });
+    } else if (releaseId) {
+      core.info(`Getting release ID: ${releaseId}`);
+      release = await octokit.rest.repos.getRelease({ owner, repo, releaseId });
+    } else {
+      core.setFailed('No valid tag or release ID provided.');
+    }
+
+    if (!release || !release.data) {
+      core.setFailed('Release does not exist or is unaccessible.');
+    }
+
+    core.info(`${release.data.assets.length} assets available.`);
+
+    core.debug(JSON.stringify(release));
+    core.setOutput('name', release.data.name);
+    core.setOutput('body', release.data.body);
+    core.setOutput('tag', release.data.tag_name);
+
+    if (release.data.assets.length === 0) {
+      core.warning('No assets available, exiting.');
+      return;
+    }
+
+    let assets = [];
+
+    if (selectorFiles.length === 1 && selectorFiles[0] === '*') {
+      core.info('Downloading all assets available');
+      assets = release.data.assets;
+    } else {
+      for (const asset of release.data.assets) {
+        for (const name of selectorFiles) {
+          const rexpr = wildcardToRegExp(name);
+          if (asset.name.match(rexpr)) {
+            assets.push(asset);
+          }
+        }
+      }
+      if (assets.length <= 0) {
+        core.setFailed('No assets selected or available.');
+      }
+      core.info(`${assets.length} assets selected.`);
+    }
+
+    const target = core.getInput('target');
+    let folder = target;
+
+    if (target) {
+      let createFolder = false;
+
+      // Extract filename from target path and get folders only
+      if (assets.length === 1 && target.includes('/')) {
+        createFolder = true;
+
+        folder = target.split('/');
+        folder.pop(); // remove filename
+        folder = folder.join('/');
+      } else if (assets.length > 1 || selectorFiles[0] === '*') {
+        createFolder = true;
+      }
+
+      if (!fs.existsSync(folder) && createFolder) {
+        core.info(`Creating folder ${folder}`);
+        fs.mkdirSync(folder, { recursive: true });
+      }
+    }
+
+    let createdAssets = [];
+
+    assets.forEach(async (asset) => {
+      let filename = asset.name;
+      let msg = `Downloading ${asset.name} with ${asset.size} bytes`;
+      if (target) {
+        filename = (assets.length === 1 ? target : `${folder}/${asset.name}`);
+        filename = filename.replace('//', '/');
+        msg += ` to ${filename}`;
+      }
+      createdAssets.push(filename);
+      core.info(msg);
+
+      const file = fs.createWriteStream(filename);
+      const response = await octokit.rest.repos.getReleaseAsset({
+        headers: { Accept: 'application/octet-stream' },
+        owner,
+        repo,
+        asset_id: asset.id,
+      });
+      core.debug(response);
+      file.write(Buffer.from(response.data));
+      file.end();
+    });
+    await core.setOutput('assets', createdAssets);
   } catch (error) {
     core.setFailed(error.message);
   }
-
-  if (!repository) { repository = context.repo.repo; }
-  core.info(`Using GitHub repository: ${repository}`);
-
-  const owner = repository.split('/')[0];
-  const repo = repository.split('/')[1];
-
-  let release = null;
-  const tag = core.getInput('tag');
-  const releaseId = core.getInput('release-id');
-
-  if (tag) {
-    core.info(`Getting release by tag: ${tag}`);
-    release = octokit.rest.repos.getReleaseByTag(owner, repo, tag);
-  } else if (releaseId && releaseId === 'latest') {
-    core.info('Getting latest release');
-    release = octokit.rest.repos.getLatestRelease(owner, repo);
-  } else if (releaseId) {
-    core.info(`Getting release ID: ${releaseId}`);
-    release = octokit.rest.repos.getRelease(owner, repo, releaseId);
-  } else {
-    core.setFailed('No valid tag or release ID provided.');
-  }
-
-  if (!release) {
-    core.setFailed('Release does not exist or is unaccessible.');
-  }
-
-  core.info(release);
 }
 
 run();
